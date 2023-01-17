a6c1aa817bf3638518c236be7742b38dbaf8f7dd
Vanzi123 <119907492+Vanzi123@users.noreply.github.com>
2023-01-17 18:24:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/home.html"));
+});
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/views/home.html');
+// });
+
+// app.get('/register', (req,res)=>{
+//     res.sendFile(__dirname + '/views/register.html');
+// });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve("./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+  });
+
+app.listen(PORT, () => {
+  console.log("server activado");
+});
